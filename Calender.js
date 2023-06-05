b10bc3a8342ef68359d96560ba4242b8233a3aca
Adarsh kushwha <adarshkushwaha692@gmail.com>
2023-06-05 14:21:38 +0530

    date issue resolved

--- Calender.js
+++ Calender.js
@@ -1,67 +1,63 @@
-let dt = new Date()
-
-let endDate = new Date(
-    dt.getFullYear(),
-    dt.getMonth() + 1,
-    0
-).getDate()
-
-function RenderDate(){
-    let today = new Date()
-dt.setDate(1)
-let day =dt.getDay();
+let dt = new Date();
 
 
-let cells =''
-for(x=day; x>0; x--){
-    cells += "<div>" + '' + "</div>"
-}
-for( let i=1; i<=endDate; i++){
-    if(i == today.getDate() && dt.getMonth()== today.getMonth()){
-        cells += "<div class='today'>" + i + "</div>"
-    }else{
-    cells += "<div>" + i + "</div>"
+
+function RenderDate() {
+  let today = new Date();
+  dt.setDate(1);
+  let day = dt.getDay();
+  let endDate = new Date(dt.getFullYear(), dt.getMonth() + 1, 0).getDate();
+
+  let cells = "";
+  for (x = day; x > 0; x--) {
+    cells += "<div>" + "" + "</div>";
+  }
+  for (let i = 1; i <= endDate; i++) {
+    if (i == today.getDate() && dt.getMonth() == today.getMonth() ) {
+      cells += "<div class='today'>" + i + "</div>";
+    } else {
+      cells += "<div>" + i + "</div>";
     }
+  }
+
+  document.getElementsByClassName("days")[0].innerHTML = cells;
 }
 
-document.getElementsByClassName("days")[0].innerHTML = cells
+function handleMonths() {
+  let selectedMonth = document.getElementById("selectMonth").value;
+  dt.setMonth(selectedMonth);
+  RenderDate();
 }
 
-function handleMonths(){
-    let selectedMonth = document.getElementById("selectMonth").value
-    dt.setMonth(selectedMonth)
-    RenderDate()
-    
-}
-
-function handleYear(){
-    let selectedYear = document.getElementById("selectYear").value
-    dt.setFullYear(selectedYear)
-    RenderDate()
+function handleYear() {
+  let selectedYear = document.getElementById("selectYear").value;
+  dt.setFullYear(selectedYear);
+  RenderDate();
 }
 
 function handleDate() {
-    let selectedDate = document.getElementById("inputDate").value;
-  
-    if (selectedDate !== "") {
-      let day = parseInt(selectedDate);
-      if (day >= 1 && day <= endDate) {
-        dt.setDate(day);
-        RenderDate();
-  
-        // Highlight the selected date with a yellow background color
-        let dayElements = document.querySelectorAll(".days div");
-        for (let i = 0; i < dayElements.length; i++) {
-          let dayElement = dayElements[i];
-          if (parseInt(dayElement.innerHTML) === day) {
-            dayElement.style.backgroundColor = "white" ? "green": "white";
-          } else {
-            dayElement.style.backgroundColor = "";
-          }
+  let selectedDate = document.getElementById("inputDate").value;
+  let lastDate = new Date(dt.getFullYear(), dt.getMonth() + 1, 0).getDate();
+
+  if (selectedDate !== "") {
+    let day = parseInt(selectedDate);
+    if (day >= 1 && day <= lastDate) {
+      dt.setDate(day);
+      RenderDate();
+
+      // Highlight the selected date with a yellow background color
+      let dayElements = document.querySelectorAll(".days div");
+      for (let i = 0; i < dayElements.length; i++) {
+        let dayElement = dayElements[i];
+        if (parseInt(dayElement.innerHTML) === day) {
+          dayElement.style.backgroundColor = "white" ? "green": "white";
+        } else {
+          dayElement.style.backgroundColor = "";
         }
-      } else {
-        alert("Invalid date. Please enter a valid date between 1 and " + endDate);
       }
+    } else {
+      alert("Invalid date. Please enter a valid date between 1 and " + endDate);
     }
   }
+}
   
